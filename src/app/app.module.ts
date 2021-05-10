353bda418da5a742ea273d03557fb497cc114f22
mich1991 <wozniak.michal91@gmail.com>
2021-05-10 17:24:06 +0100

    single cards html css added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { SingleCardComponent } from './single-card/single-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    SingleCardComponent
   ],
   imports: [
     BrowserModule,
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
+  alphabetArray:string[] = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  clickEvent(str:string): void{
+    console.log(str)
   }
 
 }
