ad65a245a6038d41b6069850f83a7d31b6656b9e
mich1991 <wozniak.michal91@gmail.com>
2021-05-14 14:21:29 +0100

    implemented Hero interface

---
+++ src/app/models/Hero.ts
@@ -0,0 +1,13 @@
+export interface Hero {
+    comics: object,
+    description: string,
+    events: object,
+    id: number,
+    modified: string,
+    name: string,
+    resourceURI: string,
+    series: object,
+    stories: object,
+    thumbnail : object,
+    urls: [],
+}
